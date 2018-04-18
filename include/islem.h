619fbfd9ebcc41b9d979e76f501344ba006b906e
emirhanaydin <emirhanaydin95@gmail.com>
2018-04-18 18:55:47 +0300

    calistir(...), girdiyi_cozumle(...) olarak adlandırıldı. girdiyi_cozumle(...) '|' karakterine göre ayrım tamamlandı.

--- include/islem.h
+++ include/islem.h
@@ -4,7 +4,10 @@
 #include <mem.h>
 #include <stdio.h>
 #include "siparis.h"
+#include "kayit.h"
 
-extern int calistir(const char *girdi, Siparis *siparis);
+#define ISLEM_BOLUM_TAMPON_BOYUTU 256
+
+extern int girdiyi_cozumle(const char *girdi, Siparis *siparis, Kayit kayit);
 
 #endif //ISLEM_H
--- src/islem.c
+++ src/islem.c
@@ -10,49 +10,48 @@ int sayi_mi(const char *girdi) {
             return 0;
     }
 
-    if (girdi[12] != NULL)
+    if (girdi[12] != '\0')
         return 0;
 
     return 1;
 }
 
-int calistir(const char *girdi, Siparis *siparis) {
-    int i;
-    char *ggirdi = strdup(girdi);
-
-    // İlk dik çizgi karakteri
-    char *bolum = strtok(ggirdi, "|");
-
-    for (i = 0; i < 4 && bolum != NULL; i++) {
-        switch (i) {
-            case 0:
-                if (!sayi_mi(bolum)) {
-                    free(ggirdi);
-                    return 0;
-                }
-
-                siparis->anahtar = strtol(bolum, NULL, 10);
-                break;
-            case 1:
-                strcpy(siparis->ad, bolum);
-                break;
-            case 2:
-                strcpy(siparis->malzeme, bolum);
-                break;
-            case 3:
-                strcpy(siparis->renk, bolum);
-                break;
-            default:
-                break;
-        }
-
-        bolum = strtok(NULL, "|");
-    }
-
-    if (i < 4) {
-        free(ggirdi);
+int kayda_ekle(const char *anahtar, const char *ad, const char *malzeme, const char *renk, Siparis *siparis) {
+    if (!sayi_mi(anahtar))
         return 0;
+
+    siparis->anahtar = strtol(anahtar, NULL, 10);
+    strcpy(siparis->ad, ad);
+    strcpy(siparis->malzeme, malzeme);
+    strcpy(siparis->renk, renk);
+
+    return 1;
+}
+
+int girdiyi_cozumle(const char *girdi, Siparis *siparis, Kayit kayit) {
+    int i;
+    char *t, *s = (char *) girdi;
+    char *bolumler[5];
+
+    for (i = 0; i < 5; i++) bolumler[i] = malloc(sizeof(char) * ISLEM_BOLUM_TAMPON_BOYUTU);
+
+    for (i = 0; i < 5; i++) {
+        t = strchr(s, '|');
+
+        if (t == '\0') break;
+
+        memcpy(bolumler[i], s, t - s);
+        s = t + 1;
     }
+
+    if (i < 1)
+        strcpy(bolumler[0], girdi);
+    else
+        strcpy(bolumler[i], s);
+
+    for (i = 0; i < 5; i++)
+        free(bolumler[i]);
+    free(bolumler);
 
     return 1;
 }
